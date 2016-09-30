196781416c26851228dcc08cb248f8934c61e57b
GhostEric <seuwanghanbin@163.com>
2016-09-30 16:51:18 +0800

    Update my_string_sort.cpp
    complete

--- my_string_sort.cpp
+++ my_string_sort.cpp
@@ -7,86 +7,59 @@ using namespace std;
 vector<string> a;
 vector<string> b;
 int flag = 0;
-int counta = 0;
-int countb = 0;
 
-void sort(vector<string> a, vector<string> b);
-
-int judge(int i, int j);
+int judge(int i, int j,int x);
 
 int main()
 {
-	//int inpt=0;
+//--------------------------------输入a[]-------------------------------------------//
 	string inpt;
 	for (int i = 0;inpt!="end" ;i++)	//a[i-1]!="end"
 	{
 		cin >> inpt;
 		a.push_back(inpt);
-		counta++;
 	}
-	counta--;
-
 //------------------------将a[i]插入b[i]中的正确位置---------------------------------//
-	
-	for (int i = 0; i < counta; i++)   
+	for (int i = 0; i < (a.size()-1); i++)   
 	{
-		//string end;
-		for (int j = 0;j < countb; j++)
+		for (int j = 0;j < b.size(); j++)
 		{
-			//如果a[i]的第一个字母小于b[j]的第一个字母
-			//将a[i]插入到b[j]之前的位置
-			if (a[i].at(0)<b[j].at(0))
-			{
-				b.insert(b.begin() + j, a[i]);
-				countb++;
+			//调用judge，找出a[i]和b[j]第x个字符开始不相同
+			int x = 0;
+			x = judge(i, j, x);
+			//如果之前的均是按string大小顺序排列，那么第一个a[i]<b[j]的位置j就应该是a[i]插入的位置
+			//如果成功将a[i]插入某个位置，将flag改为1，并跳出循环
+			if (a[i].at(x) < b[j].at(x)) {
+				b.insert(b.begin() + j, a[i]);	
 				flag = 1;
-				//end = b[countb];
 				break;
 			}
-			if (a[i].at(0) == b[j].at(0))
-			{
-				int x = judge(i,j);
-				//如果之前的均是按字母顺序排列，那么第一个a[i]<b[j]的位置j就应该是a[i]插入的位置
-				if (a[i].at(x) < b[j].at(x)) {
-					b.insert(b.begin() + j, a[i]);
-					countb++;
-					flag = 1;
-					break;
-				}
-				//下面的if判断貌似可以不要
-				if (a[i].at(x) > b[j].at(x)) {
-					continue;
-				}
-			}
 		}
-		//如果遍历b之后都没有找到b[j]>a[i],那么将a[i]插入b末尾
+		//flag为0说明遍历b之后没有找到一个b[j]>a[i],那么将a[i]插入b的末尾
 		if (flag == 0) {
 			b.push_back(a[i]);
-			countb++;
 		}
 		flag = 0;
 	}
-	
-//--------------------------------------------------------------------------------//
+//---------------------------输出b[]----------------------------------------------//
 
-	for (int i=0;i<countb;i++)
+	for (int i = 0; i<(b.size() - 1); i++)
 	{
-		cout << b[i] << endl;
+		cout << b[i] << ',';
 	}
+	cout << b[b.size() - 1] << endl;
 	
-	//a.push_back("long");
-	system("pause");
+	//system("pause");
 	return 0;
 }
 
 
-//使用递归方法，找到a[i]和b[j]两个string第一个不相同的字符a[i].at(k)和b[j].at(k)，并返回k值
-int judge(int i, int j)
+//judge,递归调用，直到找到a[i]和b[j]的第x个字符不相同，return x 
+int judge(int i, int j,int x)
 {
-	static int  k = 1;
-	if (a[i].at(k) == b[j].at(k)) {
-		k++;
-		judge(i,j);
+	if (a[i].at(x) == b[j].at(x)) {
+		x++;
+		x = judge(i, j, x);
 	}
-	else return k;
+	return x;
 }
